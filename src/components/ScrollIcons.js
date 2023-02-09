d6ba7ffb14709ea93eb4dafdde32901adb9324db
Kevin Sasse <sassek70@gmail.com>
2023-02-09 13:13:02 -0500

    icon navbar complete

--- src/components/ScrollIcons.js
+++ src/components/ScrollIcons.js
@@ -1,22 +1,63 @@
+import { useEffect, useState } from 'react'
+import {BsChevronDoubleDown, BsChevronDoubleUp} from 'react-icons/bs'
+import styled from 'styled-components'
 
 
+const ScrollIcons = ({iconImage, section}) => {
+    // let iconElement = document.getElementById(section)
+    // let iconElementYLocation = iconElement.getBoundingClientRect().y
+    const [isHovered, setIsHovered] = useState(false)
+    const [iconElement, setIconElement] = useState()
+    const [iconElementYLocation, setIconElementYLocation] = useState()
 
-const ScrollIcon = ({index, detail}) => {
+    useEffect(() => {
+        setIconElement(document.getElementById(section))
 
+    })
+    const onMouseHover = (value) => {
+        setIsHovered(isHovered => value)
+        setIconElementYLocation(iconElement.getBoundingClientRect().y)
+        if(iconElementYLocation < 0 && isHovered) {
+            return <BsChevronDoubleDown size={30}/>
+        } else if(iconElementYLocation > 0 && isHovered) {
+            return <BsChevronDoubleUp size={30}/>
+        } else {
+            return iconImage
+        }
+    }
 
+    const handleClick = (e) => {
+        e.preventDefault()
+        iconElement && iconElement.scrollIntoView({behavior: "smooth", block: "start"})
+    }
 
     return (
         <div 
-        onMouseOver={()=> mouseHover(index)}>
-        <Links href='/' onClick={e => {
-            let section = document.getElementById(detail.toString())
-            e.preventDefault()
-            section && section.scrollIntoView({behavior: "smooth", block: "start"})
-        }
-
-        }>{isHovered ? <BsChevronDoubleDown size={30}/> : index}</Links>
+        onMouseEnter={() => onMouseHover(true)}
+        onMouseLeave={() => onMouseHover(false)}>
+        <LinksIcon as='a' href='/' onClick={(e) =>handleClick(e)}>
+            {isHovered ?
+                iconElementYLocation < 0 ? <BsChevronDoubleUp size={30}/>: <BsChevronDoubleDown size={30}/>
+                :
+                iconImage
+            }   
+        </LinksIcon>
         </div>
     )
 }
 
-export default ScrollIcon
+export default ScrollIcons
+
+const LinksIcon = styled.div`
+color: #ffbf30;
+/* padding: 5px; */
+/* margin-top: 10px; */
+
+&:hover {
+    color: #9e710e;
+}
+`
+
+// const A = styled.a`
+// text-decoration-color: none;
+// `
---
+++ src/data/ScrollIconsData.js
@@ -0,0 +1,25 @@
+import {BsInfoSquare} from 'react-icons/bs'
+import {RiArticleLine, RiCodeSSlashFill} from 'react-icons/ri'
+import {IoChatboxEllipsesSharp} from 'react-icons/io5'
+
+export const icons = [
+        {
+            iconImage: <BsInfoSquare size={30} />,
+            section: "about-me"
+        },
+        {
+            iconImage: <RiCodeSSlashFill size={30}/>,
+            section: "projects"
+        },
+        {
+            iconImage: <RiArticleLine size={30}/>,
+            section: "blogs"
+        },
+        {
+            iconImage: <IoChatboxEllipsesSharp size={30}/>,
+            section: "contact"
+        },
+    ]
+  
+
+
