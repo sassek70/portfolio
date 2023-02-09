dad114b4dfa969677a4c0304798bb832510659a4
Kevin Sasse <sassek70@gmail.com>
2023-02-09 14:42:45 -0500

    sections out of view show arrows, otherwise icon

--- src/components/ScrollIcons.js
+++ src/components/ScrollIcons.js
@@ -4,26 +4,17 @@ import styled from 'styled-components'
 
 
 const ScrollIcons = ({iconImage, section}) => {
-    // let iconElement = document.getElementById(section)
-    // let iconElementYLocation = iconElement.getBoundingClientRect().y
     const [isHovered, setIsHovered] = useState(false)
     const [iconElement, setIconElement] = useState()
     const [iconElementYLocation, setIconElementYLocation] = useState()
 
     useEffect(() => {
         setIconElement(document.getElementById(section))
+    },[section])
 
-    })
     const onMouseHover = (value) => {
         setIsHovered(isHovered => value)
-        setIconElementYLocation(iconElement.getBoundingClientRect().y)
-        if(iconElementYLocation < 0 && isHovered) {
-            return <BsChevronDoubleDown size={30}/>
-        } else if(iconElementYLocation > 0 && isHovered) {
-            return <BsChevronDoubleUp size={30}/>
-        } else {
-            return iconImage
-        }
+        setIconElementYLocation(iconElementYLocations => iconElement.getBoundingClientRect().y)
     }
 
     const handleClick = (e) => {
@@ -36,13 +27,22 @@ const ScrollIcons = ({iconImage, section}) => {
         onMouseEnter={() => onMouseHover(true)}
         onMouseLeave={() => onMouseHover(false)}>
         <LinksIcon as='a' href='/' onClick={(e) =>handleClick(e)}>
-            {isHovered ?
-                iconElementYLocation < 0 ? <BsChevronDoubleUp size={30}/>: <BsChevronDoubleDown size={30}/>
+            {isHovered ? 
+                iconElementYLocation > 1 ? 
+                    <BsChevronDoubleDown size={30}/> 
+                : 
+                    iconElementYLocation < (0-1) ? 
+                        <BsChevronDoubleUp size={30}/>
+                    :
+                        iconImage
                 :
                 iconImage
             }   
         </LinksIcon>
         </div>
+
+
+
     )
 }
 
@@ -50,14 +50,8 @@ export default ScrollIcons
 
 const LinksIcon = styled.div`
 color: #ffbf30;
-/* padding: 5px; */
-/* margin-top: 10px; */
 
 &:hover {
     color: #9e710e;
 }
 `
-
-// const A = styled.a`
-// text-decoration-color: none;
-// `
