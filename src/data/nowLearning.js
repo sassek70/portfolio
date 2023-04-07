735d45f0351a9d4d2f918074d6ebaa265e698205
Kevin Sasse <sassek70@gmail.com>
2023-04-06 23:46:06 -0400

    updated now learning

--- src/data/nowLearning.js
+++ src/data/nowLearning.js
@@ -1,18 +1,23 @@
-import {SiNextdotjs, SiTypescript} from 'react-icons/si'
+import {SiCsharp, SiNextdotjs, SiTypescript} from 'react-icons/si'
+
 import { FaAws } from 'react-icons/fa'
 
 export const nowLearning = 
 [
+    // {
+    //     id: 1,
+    //     icon: <SiNextdotjs size={30}/>
+    // },
+    // {
+    //     id: 2,
+    //     icon: <SiTypescript size={30}/>
+    // },
+    // {
+    //     id: 3,
+    //     icon: <FaAws size={30}/>
+    // },
     {
-        id: 1,
-        icon: <SiNextdotjs size={30}/>
-    },
-    {
-        id: 2,
-        icon: <SiTypescript size={30}/>
-    },
-    {
-        id: 3,
-        icon: <FaAws size={30}/>
+        id: 4,
+        icon: <SiCsharp size={30}/>
     }
 ]
